87fc806fd1f3310c0ed4f94bbfe755dda577779b
Rodrigo Natan <rnattan82@gmail.com>
2023-10-25 12:21:23 -0300

    Conexão entre os deploys

--- src/Main/index.jsx
+++ src/Main/index.jsx
@@ -113,9 +113,9 @@ export default function Main() {
   const SkillDiv = () => {
 
     return (
-      <div class="tab-contents active-tab" id="skills">
+      <div className="tab-contents active-tab" id="skills">
         <ul>
-          <li><span>Front End</span><br /> HTML | CSS | JavaScript | React | Material UI | </li>
+          <li><span>Front End</span><br /> HTML | CSS | JavaScript | React | Material UI</li>
           <li><span>Back End</span><br /> Node.js | JavaScript | API REST | Java | SQL</li>
           <li><span>Técnico</span><br />Manutenção de Micro | Redes, Protocolo TCP/IP</li>
         </ul>
@@ -126,7 +126,7 @@ export default function Main() {
   const ExperienceDiv = () => {
 
     return (
-      <div class="tab-contents active-tab" id="experience">
+      <div className="tab-contents active-tab" id="experience">
         <ul>
           <li><span>2020 - 2022</span><br />Datamétrica - Atendente de Telemarketing</li>
         </ul>
@@ -138,7 +138,7 @@ export default function Main() {
   const EducationDiv = () => {
 
     return (
-      <div class="tab-contents active-tab" id="education">
+      <div className="tab-contents active-tab" id="education">
         <ul>
           <li><span>2018</span><br />IFBA - Instituto Federal de Educação Ciências e Tecnologia da Bahia</li>
           <li><span>2023</span><br />Cubos Academy - Desenvolvimento de Software</li>
@@ -151,7 +151,7 @@ export default function Main() {
   return (
     <>
       <header id="header">
-        <div class="container">
+        <div className="container">
           <nav>
             <h1 className='logo'><span className='span-logo'>R</span>odrigo</h1>
             <ul>
@@ -159,29 +159,29 @@ export default function Main() {
               <li><a href="#portfolio">Portfolio</a></li>
               <li><a href="#contact">Contato</a></li>
             </ul>
-            <i class="fas fa-bars"></i>
+            <i className="fas fa-bars"></i>
           </nav>
-          <div class="header-text">
+          <div className="header-text">
             <p>FullStack Developer</p>
             <h1>Olá, meu nome é <span className='span-header'>Rodrigo </span></h1>
           </div>
         </div>
       </header>
       <div id="about">
-        <div class="container">
-          <div class="row">
-            <div class="about-col-1">
+        <div className="container">
+          <div className="row">
+            <div className="about-col-1">
               <img className='img-user' src={User} alt='User' />
             </div>
-            <div class="about-col-2">
+            <div className="about-col-2">
 
-              <h1 class="sub-title">Sobre</h1>
+              <h1 className="sub-title">Sobre</h1>
               <p>Baiano, nascido e criado no azeite de dendê. Apaixonado por tecnologia desde guri. Curioso, obstinado e ambicioso em viver o que o mundo tem a oferecer.</p><br />
               <p>Desenvolvedor full stack em formação pela Cubos Academy. Técnico em informática pelo Instituto Federal da Bahia. Bagagem em Java e SQL.</p><br />
               <p>Analista de dados em busca de conhecimento e novas oportunidades de aprendizado, ambientes
                 onde eu possa evoluir na programação em geral e ao mesmo tempo contribuir com os meus conhecimentos.</p><br />
 
-              <div class="tab-titles">
+              <div className="tab-titles">
                 <p id="skill" className="tab-links " onClick={handleShowSkill}>Skills</p>
                 <p id="exp" className="tab-links " onClick={handleShowExperience}>Experience</p>
                 <p id="edc" className="tab-links " onClick={handleShowEducation}>Education</p>
@@ -196,36 +196,36 @@ export default function Main() {
       </div>
 
       <div id="portfolio">
-        <div class="container">
-          <h1 class="sub-title">Meus Trabalhos</h1>
-          <div class="work-list">
-            <div class="work">
+        <div className="container">
+          <h1 className="sub-title">Meus Trabalhos</h1>
+          <div className="work-list">
+            <div className="work">
               <img src={Work1} alt="Primeiro Projeto" />
-              <div class="layer">
+              <div className="layer">
                 <h2>Dindin-Bank</h2>
                 <p>Projeto estruturado pela Cubos Academy para desafio de módulo em dupla. É um website
                   onde os usuários podem fazer listagens de aquisições e ou vendas e terem um resumo de ganhos ou perdas.
                 </p>
-                <a href="https://github.com/rodrigodahora/Dindin-Bank" target='_blank'><img src={gitHub} class="fas fa-external-link-alt"/></a>
+                <a href="https://github.com/rodrigodahora/Dindin-Bank" target='_blank'><img src={gitHub} className="fas fa-external-link-alt"/></a>
               </div>
             </div>
-            <div class="work">
+            <div className="work">
               <img src={Work2} alt="" />
-              <div class="layer">
+              <div className="layer">
                 <h2>Equipe Baby #</h2>
                 <p>Projeto estruturado pela Cubos Academy para desafio de módulo em equipe. É um website
                   onde os usuários podem fazer controles bancários e gestão de cobranças dos usuários.
                   Link do deploy no README do projeto!</p>
-                <a href="https://github.com/rodrigodahora/desafio-modulo05-front-Baby-Sharp-DOM-DOM" target='_blank'><img src={gitHub} class="fas fa-external-link-alt"/></a>
+                <a href="https://github.com/rodrigodahora/desafio-modulo05-front-Baby-Sharp-DOM-DOM" target='_blank'><img src={gitHub} className="fas fa-external-link-alt"/></a>
               </div>
             </div>
             
-            <div class="work">
+            <div className="work">
               <img src={Work3} alt="" />
-              <div class="layer">
+              <div className="layer">
                 <h2>Projetos em Construção</h2>
                 <p></p>
-                <a href="#"><img src={gitHub} class="fas fa-external-link-alt"/></a>
+                <a href="#"><img src={gitHub} className="fas fa-external-link-alt"/></a>
               </div>
             </div>
           </div>
@@ -237,15 +237,15 @@ export default function Main() {
           <div className="row">
             <div className="contact-left">
               <h2 className="sub-title">Contato</h2>
-              <p classNameName='p-telefone'><img src={telefone} className="fas fa-phone-square-alt img-icons"></img> (75) 9 9894-1934</p>
+              <p className='p-telefone'><img src={telefone} className="fas fa-phone-square-alt img-icons"></img> (75) 9 9894-1934</p>
               <div className="social-icons">
                 <a href="https://www.facebook.com/rodrigo.natan.9/" target='_blank'><img className='fab fa-facebook img-icons' src={facebook} alt="Facebook" /></a>
                 <a href="https://www.instagram.com/natandh/" target='_blank'><img className='fab fa-instagram img-icons' src={instagram} alt="Instagram" /></a>
                 <a href="https://www.linkedin.com/in/natandh/" target='_blank'><img className='fab fa-linkedin img-icons' src={linkedin} alt="Instagram" /></a>
               </div>
-              <a href={CV} download class="btn btn2">Download Currículo</a>
+              <a href={CV} download className="btn btn2">Download Currículo</a>
             </div>
-            <div class="contact-right">
+            <div className="contact-right">
               <form>
                 <input type="text" name="Name" placeholder="Your Name" onChange={(event) => setNome(event.target.value)} required />
                 <input type="text" name="Assunto" placeholder="Subject" onChange={(event) => setAssunto(event.target.value)} required />
@@ -259,8 +259,8 @@ export default function Main() {
           </div>
         </div>
         <a href="#" ><img id='btn-top' src={top} alt="Back to the top"/></a>
-        <footer class="copyright">
-          <p>copyright @ Dev Alphaspace. Made with <i class="fas fa-heart"></i> by Dev Alphaspace</p>
+        <footer className="copyright">
+          <p>copyright @ Dev Alphaspace. Made with <i className="fas fa-heart"></i> by Dev Alphaspace</p>
         </footer>
       </div >
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: 'https://api-dahora-smtp.onrender.com/',
     timeout: 10000,
     headers: {
         'Content-Type': 'Application/json',
