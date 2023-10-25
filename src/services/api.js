b4971bfd0a449bc3f4cbd024f8b3a4cb6b40aa94
Rodrigo Natan <rnattan82@gmail.com>
2023-10-25 12:33:14 -0300

    Alterando api no front

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
     baseURL: 'https://api-dahora-smtp.onrender.com/',
     timeout: 10000,
     headers: {
-        'Content-Type': 'Application/json',
+        "Content-Type": "application/json",
     },
 });
 
